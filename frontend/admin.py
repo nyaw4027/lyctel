4d981938c8ebdd5c3c11863461056f1a9f7e3bab
nyaw4027 <nyaw4027@gmail.com>
2026-05-18 22:43:46 +0000

    updated the templates

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import AboutPage, AboutStat, AboutFeature, TeamMember
+
+class StatInline(admin.TabularInline):
+    model = AboutStat
+    extra = 1
+
+class FeatureInline(admin.TabularInline):
+    model = AboutFeature
+    extra = 1
+
+class TeamInline(admin.TabularInline):
+    model = TeamMember
+    extra = 1
+
+
+@admin.register(AboutPage)
+class AboutPageAdmin(admin.ModelAdmin):
+    inlines = [StatInline, FeatureInline, TeamInline]
