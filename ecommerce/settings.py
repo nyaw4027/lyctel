03681ef7ace2309bbe8182b71cfe2763f9b5fcbd
nyaw4027 <nyaw4027@gmail.com>
2026-05-20 23:46:45 +0000

    update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-CHANGE_ME')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
