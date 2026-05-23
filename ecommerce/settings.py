0d2b94eb08c7aa4400076942f4b5185169ef49c7
nyaw4027 <nyaw4027@gmail.com>
2026-05-23 16:35:51 +0000

    updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','lynctel.up.railway.app']
 
