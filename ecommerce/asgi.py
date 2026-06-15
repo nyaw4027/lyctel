42786f057c152edde13cb509ef4553807edd296b
nyaw4027 <nyaw4027@gmail.com>
2026-06-15 06:30:24 +0000

    Wire daphne, asgi, chat urls

--- ecommerce/asgi.py
+++ ecommerce/asgi.py
@@ -3,37 +3,11 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-import delivery.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            delivery.routing.websocket_urlpatterns
-        )
-    ),
-})
-
-
-
-
-
-
-# ecommerce/asgi.py
-# Replace your existing asgi.py with this
-
-import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 django_asgi_app = get_asgi_application()
 
-from chat.routing import websocket_urlpatterns  # noqa: E402
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
