3db01e7c2ad85320a07459d87ddabf1407d15c2f
nyaw4027 <nyaw4027@gmail.com>
2026-06-27 23:49:17 +0000

    products videos updated

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.product_list, name='list'),
-    path("deals/", views.deals_page, name="deals"),
-    path('<slug:slug>/', views.product_detail, name='detail'),
-   
+    path('',                        views.product_list,   name='list'),
+    path('deals/',                  views.deals_page,     name='deals'),
+    path('<slug:slug>/',            views.product_detail, name='detail'),
+    path('video/<int:pk>/delete/',  views.video_delete,   name='video_delete'),
 ]
