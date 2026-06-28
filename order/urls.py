9257e374fe83d9f1eb17a82cb4b4f3f7a417768f
nyaw4027 <nyaw4027@gmail.com>
2026-06-28 10:07:22 +0000

    upgrade the delivery

--- order/urls.py
+++ order/urls.py
@@ -4,9 +4,10 @@ from . import views
 app_name = 'order'
 
 urlpatterns = [
-    path('checkout/',                        views.checkout,           name='checkout'),
-    path('',                                 views.order_history,      name='history'),
-    path('<str:order_ref>/confirm/',         views.order_confirmation, name='confirmation'),
-    path('<str:order_ref>/track/',           views.order_tracking,     name='tracking'),
-   # path('api/list/', views.api_order_history, name='api_order_history'),
+    path('checkout/',                          views.checkout,               name='checkout'),
+    path('',                                   views.order_history,          name='history'),
+    path('<str:order_ref>/confirm/',           views.order_confirmation,     name='confirmation'),
+    path('<str:order_ref>/track/',             views.order_tracking,         name='tracking'),
+    path('confirm-pickup/<str:order_ref>/',    views.vendor_confirm_pickup,  name='confirm_pickup'),
+    path('dispatch-parcel/<str:order_ref>/',   views.vendor_dispatch_parcel, name='dispatch_parcel'),
 ]
