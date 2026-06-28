8fe551609673e548e0c29e32a140c5c4d087a265
nyaw4027 <nyaw4027@gmail.com>
2026-06-28 22:49:34 +0000

    live stream full update

--- ecommerce/asgi.py
+++ ecommerce/asgi.py
@@ -7,11 +7,21 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 django_asgi_app = get_asgi_application()
 
-from chat.routing import websocket_urlpatterns
+# Import all WebSocket URL patterns
+from chat.routing     import websocket_urlpatterns as chat_ws
+from delivery.routing import websocket_urlpatterns as delivery_ws
+from livestream.routing import websocket_urlpatterns as livestream_ws
+
+# Combine all WebSocket routes into one router
+all_websocket_urlpatterns = (
+    chat_ws +
+    delivery_ws +
+    livestream_ws
+)
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+        URLRouter(all_websocket_urlpatterns)
     ),
 })
