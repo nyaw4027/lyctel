2425f30bb6ff9769aced89c6031be8a175f008f8
nyaw4027 <nyaw4027@gmail.com>
2026-07-08 22:56:17 +0000

    profile fix

--- products/apps.py
+++ products/apps.py
@@ -1,5 +1,14 @@
+# products/apps.py
 from django.apps import AppConfig
 
-
 class ProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
+
+    def ready(self):
+        try:
+            from pillow_heif import register_heif_opener
+        except ImportError:
+            return
+
+        register_heif_opener()
