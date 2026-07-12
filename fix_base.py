3d4e19ae6d43dd9390634de204d86dde90c72427
nyaw4027 <nyaw4027@gmail.com>
2026-07-12 11:10:19 +0000

    updates on the css

---
+++ fix_base.py
@@ -0,0 +1,29 @@
+﻿import re
+
+path = "templates/base.html"
+content = open(path, encoding="utf-8").read()
+original_len = len(content)
+
+content = re.sub(
+    r"<!-- ── ANTI-FLASH VEIL.*?</script>\s*",
+    "",
+    content,
+    flags=re.DOTALL
+)
+
+content = re.sub(
+    r"<!-- FIXED: the Tailwind CDN script tag.*?window\.tailwind\.config = \{.*?\};\s*</script>\s*",
+    "",
+    content,
+    flags=re.DOTALL
+)
+
+if "css/output.css" not in content:
+    content = content.replace(
+        "<link rel=\"shortcut icon\" href=\"{% static 'icons/icon-96x96.png' %}\"/>",
+        "<link rel=\"shortcut icon\" href=\"{% static 'icons/icon-96x96.png' %}\"/>\n\n  <link rel=\"stylesheet\" href=\"{% static 'css/output.css' %}\"/>"
+    )
+
+open(path, "w", encoding="utf-8").write(content)
+print("Removed", original_len - len(content), "characters of dead CDN/veil code")
+print("output.css link now present:", "css/output.css" in content)
---
+++ fix_dup.py
@@ -0,0 +1,7 @@
+﻿content = open('templates/base.html', encoding='utf-8').read()
+
+# Remove the first (duplicate) occurrence only
+dup = '<link rel="stylesheet" href="{% static \'css/output.css\' %}"/>\n  <link rel="stylesheet" href="{% static \'css/main.css\' %}"/>\n  <link rel="stylesheet" href="{% static \'css/uploads.css\' %}"/>\n'
+content = content.replace(dup, '', 1)
+open('templates/base.html', 'w', encoding='utf-8').write(content)
+print('Done')
