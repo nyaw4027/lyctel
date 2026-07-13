3ed870b3d7d4b0b3a3ea5ce5b5435a78eceb9a3b
agyesam12 <agyesam12@gmail.com>
2026-07-13 02:13:49 +0000

    fixed some files and then removed tailwing css

--- fix_cdn.py
+++
@@ -1,5 +0,0 @@
-﻿content = open('templates/base.html', encoding='utf-8').read()
-# Remove Tailwind CDN script
-content = content.replace('<script src="https://cdn.tailwindcss.com" defer></script>', '')
-open('templates/base.html', 'w', encoding='utf-8').write(content)
-print('Done')
